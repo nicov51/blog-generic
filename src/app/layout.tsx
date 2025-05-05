76233253a502c3b7117dfa52ad1e0960a6f4325c
nicov51 <nicolas.viennot@laposte.net>
2025-05-05 09:07:25 +0200

    feat(layout): remove use client and paths admin

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,36 +1,36 @@
-"use client"
-
 import './globals.css';
 import Navbar from '@/components/Navbar';
 import Footer from '@/components/Footer';
 import Header from '@/components/Header';
 import React from "react";
 import SessionWrapper from "@/lib/SessionWrapper";
-import { usePathname }  from "next/navigation";
+
+
+export const metadata = {
+  title: "Nom de ton site",
+  description: "Description ici",
+};
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
 
-  const pathname = usePathname();
-  const isAdminRoute = pathname.startsWith("/adminDashboard");
-
   return (
-    <SessionWrapper>
+
     <html lang="fr">
     <body className="bg-white text-black">
-    {/* Si ce n'est PAS une page Admin, on affiche Navbar */}
-    {!isAdminRoute && <Navbar />}
-    {!isAdminRoute && <Header />}
+    <SessionWrapper>
 
+      < Navbar />
+      < Header />
 
-    {/* Main content */}
-    <main className={`${!isAdminRoute ? "max-w-6xl mx-auto px-4 py-6" : ""}`}>
+    <main className= "max-w-6xl mx-auto px-4 py-6">
       {children}
     </main>
 
-    {/* Si ce n'est PAS une page Admin, on affiche Footer */}
-    {!isAdminRoute && <Footer />}
+    <Footer />
+
+    </SessionWrapper>
     </body>
     </html>
-    </SessionWrapper>
+
   );
 }
