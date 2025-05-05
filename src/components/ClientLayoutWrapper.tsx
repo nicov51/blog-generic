d011272e3445e9ddf0777cd42f2fc00a09a0f2de
nicov51 <nicolas.viennot@laposte.net>
2025-05-05 19:53:30 +0200

    feat(SEO):add meta homePage and delete layout Admin

--- src/components/ClientLayoutWrapper.tsx
+++ src/components/ClientLayoutWrapper.tsx
@@ -4,9 +4,29 @@ import Navbar from '@/components/Navbar';
 import Footer from '@/components/Footer';
 import Header from '@/components/Header';
 import SessionWrapper from '@/lib/SessionWrapper';
+import SidebarAdmin from '@/components/SidebarAdmin';
 import React from "react";
+import { Box } from '@mui/material';
+import { usePathname } from 'next/navigation';
 
 export default function ClientLayoutWrapper({ children }: { children: React.ReactNode }) {
+  const pathname = usePathname();
+  const isAdmin =pathname.startsWith("/adminDashboard");
+
+  if (isAdmin) {
+    return (
+      <SessionWrapper>
+        <Box sx={{ display: "flex", minHeight: '100vh' }}>
+          <SidebarAdmin />
+          <Box sx={{ flexGrow: 1, p: 3 }}>
+            {children}
+          </Box>
+        </Box>
+      </SessionWrapper>
+    );
+  }
+
+
   return (
     <SessionWrapper>
       <Navbar />
